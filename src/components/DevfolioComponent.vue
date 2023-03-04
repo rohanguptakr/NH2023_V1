d4f5596b49ee509644808293e684604229963164
steffiihihi <118903812+steffiihihi@users.noreply.github.com>
2023-03-04 17:29:54 +0530

    Update DevfolioComponent.vue

--- src/components/DevfolioComponent.vue
+++ src/components/DevfolioComponent.vue
@@ -13,14 +13,36 @@
           </p>
         </a>
       </p>
-    </div>
-    <div
+      <div
       class="apply-button devfolio"
       ref="button-devfolio"
       data-hackathon-slug="nmithacks"
       data-button-theme="light"
       :style="{ height: `10px`, width: `312px`, padding: `2vh` }"
-    ></div>
+      >
+      </div>
+      <div class="countdown-wrapper" style="margin-top: 35px;">
+        <p id="timerHeading">Registration ends in:</p>
+        <div class="countdown">
+         <div class="countdown-item">
+            <span>{{ days }}</span>
+            <p>Days</p>
+          </div>
+         <div class="countdown-item">
+            <span>{{ hours }}</span>
+            <p>Hours</p>
+          </div>
+         <div class="countdown-item">
+            <span>{{ minutes }}</span>
+            <p>Minutes</p>
+          </div>
+         <div class="countdown-item">
+            <span>{{ seconds }}</span>
+            <p>Seconds</p>
+          </div>
+          </div>
+        </div>
+      </div>    
     <p class="end-date">Registration ends on: 15th March,2023</p>
   </div>
 </template>
@@ -42,11 +64,105 @@ export default {
       };
     }, []);
   },
+  data() {
+    return {
+      deadline: new Date("2023-03-15"),
+      countdownInterval: null,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  },
+  mounted() {
+    this.startCountdown();
+    window.addEventListener("resize", this.handleResize);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    startCountdown() {
+      this.countdownInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = this.deadline.getTime() - now;
+
+        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        if (distance < 0) {
+          clearInterval(this.countdownInterval);
+          this.days = 0;
+          this.hours = 0;
+          this.minutes = 0;
+          this.seconds = 0;
+        }
+      }, 1000);
+    },
+    handleResize() {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth < 768) {
+        this.$el.querySelector(".countdown").classList.add("responsive");
+      } else {
+        this.$el.querySelector(".countdown").classList.remove("responsive");
+      }
+    },
+  },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
+.countdown-wrapper {
+  text-align: center;
+}
+
+.countdown {
+  display: flex;
+  justify-content: center;
+}
+
+.countdown-item {
+  margin: 0 10px;
+  text-align: center;
+}
+
+.countdown-item span {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.countdown-item p {
+  font-size: 0.8rem;
+  text-transform: uppercase;
+}
+
+@media (max-width: 767px) {
+  .countdown.responsive {
+    display: flex;
+    justify-content: center;
+  }
+
+  .countdown-item {
+  margin: 0 10px;
+  text-align: center;
+  }
+  .countdown-item span {
+    font-size: 0.7rem;
+    font-weight: bold;
+  }
+
+  .countdown-item p {
+    font-size: 0.34rem;
+    text-transform: uppercase;
+  }
+  #timerHeading{
+    font-size:12px;
+  }
+}
 h3 {
   position: relative;
   top: 29vh;
